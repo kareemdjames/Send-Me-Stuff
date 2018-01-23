dc902777abf2cc404bc5a818f03cd0ea9b0126eb
Kareem James <kareemdjames@gmail.com>
2018-01-22 23:08:02 -0500

    able to get title of article from meta data and display

--- routes/tags.js
+++ routes/tags.js
@@ -3,6 +3,7 @@ const turbo = require('turbo360')({site_id: process.env.TURBO_APP_ID})
 const vertex = require('vertex360')({site_id: process.env.TURBO_APP_ID})
 const router = vertex.router()
 const superagent = require('superagent')
+const cheerio = require('cheerio')
 
 // Route to get meta tags from url
 router.get('/', function(req, res){
@@ -16,7 +17,7 @@ router.get('/', function(req, res){
     return 
   }
 
-  // Request HTML from the url, then displays that html from our website
+  // Request(Grabs) HTML from the url, then displays that html from our local server
   superagent
   .get(url)
   .query(null)
@@ -28,8 +29,35 @@ router.get('/', function(req, res){
       })
       return
     }
+
+    const tags = {}
+
     const html = response.text
-    res.send(html)
+    // Cheerio allows us to parse the HTML via jQuery
+    // Parsing out the meta data from the HTML
+    $ = cheerio.load(html)
+    // loop through all the meta tags and return them
+    $('meta').each(function(i, meta) {
+      const attribs = meta.attribs
+      // if the meta tag has doesn't have attributes return
+      if (attribs  == null)
+        return true
+
+      const property = attribs.property
+      // if the attribue doesnt have a property return
+      if (property == null) 
+        return true
+        
+      if (property == 'og:title') {
+        tags['title'] = attribs.content
+      }
+    })
+    
+    res.json({
+      confirmation: 'success',
+      tags: tags
+    })
+    // res.send(html)
   })
   
 })
