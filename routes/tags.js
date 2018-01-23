7c13f88f4f4220bb9d0d30f3f99f9d29a54bd730
Kareem James <kareemdjames@gmail.com>
2018-01-22 22:19:48 -0500

    tag route works, able to show json

--- routes/tags.js
+++ routes/tags.js
@@ -4,6 +4,16 @@ const vertex = require('vertex360')({site_id: process.env.TURBO_APP_ID})
 const router = vertex.router()
 
 router.get('/', function(req, res){
+  const url = req.query.url
+
+  if (url == null) {
+    res.json({
+      confirmation: 'fail',
+      message: 'missing url query parameter!'
+    })
+    return 
+  }
+  
   res.json({
     confirmation: 'success',
     data: 'this is the tags route!'
